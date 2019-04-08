0c401a5e2f94e324625146965a33523ed21ab9b8
thankcreate <thankcreate@gmail.com>
2019-04-07 23:37:32 -0400

    rename prj jump

--- scenes/chuang_xie_7_jump/scene.lua
+++ scenes/chuang_xie_7_jump/scene.lua
@@ -45,15 +45,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
 -- Create scene-group and camera (for drawing the pattern using the canvas texture)
 
 sceneCamera = createObjectCamera(100, vector(0, 0), rootObject)
@@ -79,7 +70,7 @@ cubeObject = createObjectShape(cubeMesh, vector(pos.x, pos.y, 10), textureGroup)
 cubeObject.scale = scale
 function cubeObject:update(dt)
 	self.position = vector(pos.x + offset.x, pos.y + offset.y, 10)
-	-- self.scale = vector(scale.x * scaleAdjust.x, scale.y * scaleAdjust.y, scale.z * scaleAdjust.z)
+	self.scale = vector(scale.x * scaleAdjust.x, scale.y * scaleAdjust.y, scale.z * scaleAdjust.z)
 end
 
 clipOffset = vector(0, 0);
@@ -89,7 +80,7 @@ cubeObjectClip.scale = scale
 
 function cubeObjectClip:update(dt)
 	self.position = vector(pos.x + offset.x, pos.y + offset.y, 10)
-	-- self.scale = vector(scale.x * scaleAdjust.x, scale.y * scaleAdjust.y, scale.z * scaleAdjust.z)
+	self.scale = vector(scale.x * scaleAdjust.x, scale.y * scaleAdjust.y, scale.z * scaleAdjust.z)
 end
 
 
@@ -176,133 +167,14 @@ buttonColorLabel.color = color(0.3, 0.3, 0.3, 1)
 buttonAuto.color = color(0.3, 0.3, 0.3, 1)
 buttonManual.color = color(0.3, 0.3, 0.3, 1)
 
-phaseIndex = 1
--- lua array start with index 1
-phaseArray = {
-	vector(-82.5, 38.75, 1), 	-- null
-	vector(-81.25, 37.5,1),  	-- dot
-	vector(-78.75, 35, 1), 		-- big dot
-	vector(-71.25, 35, 1),		-- two dot
-	vector(-71.25, 23.75, 1),
-	vector(-71.25, 18.75, 1),
-	vector(-56.25, 18.75, 1),
-	vector(-56.25, 2.5, 1),
-	vector(0, 2.5, 1),
-	vector(0, -7.5, 1),
-	vector(0, -7.5, 1.3),
-	vector(0, -7.5, 2.3),
-	vector(-8, 23.75, 6),
-
-	nil
-}
-
-timeTable = {
-	vector(0, 10),
-	vector(13, 13.5),			-- show dot
-	vector(18.5, 24.4),			-- show bigger dot
-	vector(35.8, 38.6), 		-- show two dot
-	vector(42, 47), 			-- show H
-	vector(59, 60), 			-- show H Sep
-	vector(64.8, 68),			-- show 4 leg
-	vector(70.5, 72),			-- show 2 bug
-	vector(76, 78),				-- show 4 love 2d
-	vector(79, 80),				-- x move
-	vector(81.4, 83),				-- scale big
-	vector(83.7, 89),				-- scale very big
-	vector(89, 100)				-- scale very big
-
-}
-
-function updateStateUsingTime()
-
-	if time < timeTable[2].x then
-		updateState(phaseArray[1])
-		return
-	end
-
-	
-
-	for i = 2, #timeTable do
-		local prevIt = timeTable[i - 1]
-		local prevEndTime = prevIt.y	
-
-		local it = timeTable[i]
-		local startTime = it.x
-		local endTime = it.y
-
-		if time >= prevEndTime and time < startTime then
-			updateState(phaseArray[i - 1])
-			break
-		end
-
-		if time >= startTime and time <= endTime then
-			local progress = (time - startTime) / (endTime - startTime)
-			local newX = phaseArray[i - 1].x + (phaseArray[i].x - phaseArray[i - 1].x) * progress
-			local newY = phaseArray[i - 1].y + (phaseArray[i].y - phaseArray[i - 1].y) * progress
-			local newZ = phaseArray[i - 1].z + (phaseArray[i].z - phaseArray[i - 1].z) * progress
-
-			updateState(vector(newX, newY, newZ))
-			break
-		end
-	end
-end
-
-function updateState(iter)
-	offset = vector(iter.x, iter.y)
-	scaleAdjust = vector(iter.z, iter.z, iter.z)
-	updateSliders()
-end
-
-function updatePhaseData()
-	local iter = phaseArray[phaseIndex]
-	updateState(iter)
-end
-
-updatePhaseData()
 
 
-function buttonColorNext:action()
-	if phaseArray[phaseIndex + 1] ~= nil then
-		phaseIndex = phaseIndex + 1
-	end
-	
-	updatePhaseData()
 
-end
-
-function buttonColorPrev:action()
-	phaseIndex = phaseIndex - 1
-	if phaseIndex < 1 then
-		phaseIndex = 1
-	end
-	updatePhaseData()
-
-end
-
-function buttonAuto:action()
-	manualMode = false
-end
-
-function buttonManual:action()
-	manualMode = true
-end
-
-
-function updateCamScale()
-	globalCameraMain:setViewSize(100 / scaleAdjust.x)
-end
 
 function rootObject:update(dt)
 	time = time + dt
 
-	if manualMode then
-
-	else
 	
-		updateStateUsingTime()
-	end
-	
-	updateCamScale()
 
 	-- print(time)
 	textLine3 = offset.x  .. "     " .. offset.y .. "    " .. scaleAdjust.x
@@ -316,6 +188,6 @@ function rootObject:update(dt)
 
 end
 
-src1 = love.audio.newSource(globalScenePath .."music/2001.mp3", "static") 
-src1:setVolume(0.9) -- 90% of ordinary volume
-src1:play()
+-- src1 = love.audio.newSource(globalScenePath .."music/2001.mp3", "static") 
+-- src1:setVolume(0.9) -- 90% of ordinary volume
+-- src1:play()
